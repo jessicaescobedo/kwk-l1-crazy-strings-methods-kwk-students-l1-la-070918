c6f4b8504a6e8b2d650a0d9b3d7e69aa7cd22688
jessicaescobedo <jessicaivetteescobedo@gmail.com>
2018-07-10 17:56:23 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -3,11 +3,11 @@ second_word= "Jess"
 def crazy_strings(first_word, second_word)
 puts "#{first_word.upcase.reverse} #{second_word}!"
 end
-crazy_strings(first_word, second_word)
+crazy_strings("Hi", "Jess")
 
 # first_word= "Hi"
 # second_word= "Jess"
 def crazy_strings(first_word, second_word)
 puts "#{first_word} #{second_word.gsub("s","z").swapcase}!"
 end
-crazy_strings(first_word, second_word)
+crazy_strings("Hi", "Jess")
