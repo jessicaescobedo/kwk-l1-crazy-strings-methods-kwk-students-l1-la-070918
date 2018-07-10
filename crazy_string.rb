191dc4c0115b917eb03696cdb6160c0e2055c337
jessicaescobedo <jessicaivetteescobedo@gmail.com>
2018-07-10 17:50:57 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,7 +1,7 @@
 first_word= "Sam"
 second_word= "Jess"
 def crazy_strings(first_word, second_word)
-puts "Hello #{first_word.upcase} My name's #{second_word}!"
+puts "Hello #{first_word.reverse.upcase} My name's #{second_word}!"
 end
 crazy_strings(first_word, second_word)
 
