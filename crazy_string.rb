5c5214098572fd213235255a3594049bb8cc09ef
jessicaescobedo <jessicaivetteescobedo@gmail.com>
2018-07-10 17:58:33 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -3,11 +3,11 @@
 def crazy_strings(first_word, second_word)
 puts "#{first_word.upcase.reverse} #{second_word}!"
 end
-crazy_strings("Hi", "Jess")
+crazy_strings("Apple", "Jess")
 
 # first_word= "Hi"
 # second_word= "Jess"
 def crazy_strings(first_word, second_word)
 puts "#{first_word} #{second_word.gsub("s","z").swapcase}!"
 end
-crazy_strings("Hi", "Jess")
+crazy_strings("Apple", "Jess")
